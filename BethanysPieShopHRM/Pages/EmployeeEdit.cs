25c99aea33ef2ce2fcd598c93bd94fef90b526a7
GCostill <gaby123le@gmail.com>
2022-06-03 15:04:58 -0400

    added JobCategory to EmployeeEdit.cs

--- BethanysPieShopHRM/Pages/EmployeeEdit.cs
+++ BethanysPieShopHRM/Pages/EmployeeEdit.cs
@@ -14,6 +14,8 @@ namespace BethanysPieShopHRM.Pages
         public IEmployeeDataService EmployeeDataService { get; set; }
         [Inject]
         public ICountryDataService CountryDataService { get; set; }
+        [Inject]
+        public IJobCategoryDataService JobCategoryDataService { get; set; }
         [Parameter]
         public string EmployeeId { get; set; }
 
@@ -21,14 +23,19 @@ namespace BethanysPieShopHRM.Pages
 
         public List<Country> Countries { get; set; } = new List<Country>();
 
+        public List<JobCategory> JobCategories { get; set; } = new List<JobCategory>();
+
         protected string CountryId = string.Empty;
+        protected string JobCategoryId = string.Empty;
 
         protected override async Task OnInitializedAsync()
         {
             Countries = (await CountryDataService.GetAllCountries()).ToList();
             Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+            JobCategories = (await JobCategoryDataService.GetAllJobCategories()).ToList();
 
             CountryId = Employee.CountryId.ToString();
+            JobCategoryId = Employee.JobCategoryId.ToString();
         }
     }
 }
